33c9a74d037b91d3a29575750b97c050a734abea
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-02-25 00:57:19 +0000

    add redirect directive in index php file

--- public/index.php
+++ public/index.php
@@ -1,5 +1,12 @@
 <?php
 
+//TODO "ssl": true, in config file
+// Redirect browser to ssl webpage
+if($_SERVER['HTTPS'] != "on") {
+	header("Location: https://xn--stio-vpa.pt/frontgate/");
+	exit;
+}
+
 // settings file
 $json = 'default.json';
 
