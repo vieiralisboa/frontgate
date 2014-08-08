f56d839b95b3a41f801280537184c48a7dbf72a5
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-08-08 06:16:25 +0100

    Update PHP Bars

--- private/bars.php
+++ private/bars.php
@@ -1,5 +1,4 @@
 <?php
-//bars.php
 
 // HTML template
 bars('index.html', $json);
@@ -8,44 +7,26 @@ bars('index.html', $json);
  * Bars
  * requires Bar class
  */
-function bars($HTML, $JSON)
-{
-    // path to this script
+function bars($HTML, $JSON){
+    // this script's parent folder
     $PATH = dirname(__FILE__) . "/";
 
     // template
-    if(file_exists("$PATH/html/$HTML"))
-    {
+    if(file_exists("$PATH/html/$HTML")){
         $html = file_get_contents("$PATH/html/$HTML");
     }
 
     // get json data
-    if(file_exists("$PATH/json/$JSON"))
-    {
+    if(file_exists("$PATH/json/$JSON")){
         $json = file_get_contents("$PATH/json/$JSON");
     }
 
     // renders the page contents
-    if(!empty($html) && !empty($json))
-    {
+    if(!empty($html) && !empty($json)){
         // decodes json data
         $config = json_decode($json);
 
-        // noscript wrap body contents?
-        $config->noscript = true;
-
-        // html data attributes
-        $data = array();
-
-        // Remote location is enabled
-        if(!$config->remote->disabled)
-        {
-            // get Remote Location headers
-            $headers = get_headers("http://{$config->remote->hostname}/index.html", 1);
-            //die("https://{$config->remote->hostname}/index.html");
-        }
-
-        // user agent SO
+        // detect user agent SO
         $OS = array('Windows', 'Win', 'Android', 'Linux', 'Mac', 'X11');
         foreach ($OS as $os) {
             $pos = stripos(strtolower($_SERVER['HTTP_USER_AGENT']), strtolower($os));
@@ -55,27 +36,52 @@ function bars($HTML, $JSON)
             }
         }
 
-        // received headers from the server (Remote location is online)
-        if($headers)
-        {
-            $config->data->remote_hostname = $config->remote->hostname;
-            $config->data->remote_protocol = $config->remote->protocol;
-            $host = $config->remote->protocol."//".$config->remote->hostname;
-            $config->data->remote_host = $host;
-            $config->data->host = $config->host;
+        // html data attributes
+        $data = array();
 
-            $data[] = str_replace('X', preg_match('/^HTTP\/\d\.\d\s+(200|301|302|404)/', $headers[0]), 'data-remote_status="X"');
+        $config->noscript = false;
+
+        $remote = 0;
+
+        // Remote location is enabled
+        if(!$config->remote->disabled){
+            // get Remote Location headers
+            $headers = get_headers($config->remote->script, 1);
+            if($headers){
+                $remote = preg_match('/^HTTP\/\d\.\d\s+(200|301|302)/', $headers[0]);
+            }
+            // Remote location is online
+            if($remote){
+                // to wrap body contents with noscript (fallback for js disabled browsers)
+                $config->noscript = true;
+                // to load remote script
+                if(isset($config->remote->script)) {
+                    $config->head->script = $config->remote->script;
+                }
+            }
         }
-        else $config->noscript = false;// remote location is offline (or disabled)
+        $data[] = str_replace('X', $remote, 'data-remote="X"');
 
-        // get snapshot
+        // Situs is enabled
+        if(!$config->situs->disabled){
+            $config->data->situs_hostname = $config->situs->hostname;
+            $config->data->situs_protocol = $config->situs->protocol;
+            $host = $config->situs->protocol."//".$config->situs->hostname;
+            if(!empty($config->situs->port)){
+                $config->data->situs_port = $config->situs->port;
+                $host .= ":".$config->situs->port;
+            }
+
+            $config->data->situs = $host;
+            $config->data->host = $config->host;
+        }
+
+        // get shebang snapshot
         include "shebang.php";
 
         // data attributes on html element
-        foreach ($config->data as $key => $value)
-        {
-            if($key != "_")
-            {
+        foreach ($config->data as $key => $value){
+            if($key != "_"){
                 $data[] = str_replace('X', $value, "data-$key=\"X\"");
             }
         }
@@ -95,26 +101,22 @@ function bars($HTML, $JSON)
         $html = str_replace("<!-- bars -->", $bars, $html);
 
         // template values
-        if(empty($config->head->lang))
-        {
+        if(empty($config->head->lang)){
             // language
             $config->head->lang = "en";
         }
 
-        if(empty($config->head->script))
-        {
+        if(empty($config->head->script)){
             // javascript
             $config->head->script = "";
         }
 
-        if(empty($config->head->stylesheet))
-        {
+        if(empty($config->head->stylesheet)){
             // stylesheet
             $config->head->stylesheet = "";
         }
 
-        foreach($config->head as $name => $value)
-        {
+        foreach($config->head as $name => $value){
             // set template value
             $html = str_replace("%$name%", $value, $html);
         }
@@ -125,8 +127,7 @@ function bars($HTML, $JSON)
 
     // template and/or data not found
     $file = "$PATH/html/404.html";
-    if(file_exists($file))
-    {
+    if(file_exists($file)){
         die(file_get_contents($file));
     }
 }
