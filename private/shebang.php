d42dacb5a1bea0ea2dd8f7ed94c9e05ef1d6eb61
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-08-21 23:37:25 +0100

    Update Shebang class

--- private/shebang.php
+++ private/shebang.php
@@ -11,42 +11,37 @@ shebang($config);
 function shebang(&$config){
 	// directory for Bar addon (json) files
 	$dir = $config->shebang->dir;
-	
-	// Default directory for Bar addon (json) files
-	if(!$dir)
-	{
+
+	// Default directory for json bar files
+	if(!$dir){
 		$dir = dirname(__FILE__) . "/json/";
-	} 
+	}
 
 	if(preg_match("/^\/\?_escaped_fragment_\=(.*)?/",
-		 urldecode($_SERVER['REQUEST_URI']), $matches))
-	{		
+			urldecode($_SERVER['REQUEST_URI']), $matches)){
 		$name = urldecode($matches[1]);
-		
-		if(empty($name))
-		{
+
+		if(empty($name)){
 			$name = "Home";
-		} 
-		
-		if($config->shebang->home && $name == "Home")
-		{
-			$dir = $config->shebang->home;
-		} 
-		
+		}
+
+		if($name == "Home"){
+			if(!empty($config->shebang->home)) $dir = $config->shebang->home;
+			else $dir = dirname(__FILE__) . "/json/";
+		}
+
 		#$url = $dir . "bar.$name.json";// works on linux
 		$url = $dir . utf8_decode("bar.$name.json");// works on windows
 
 		// $url is a file path or a non secure url (no ssl) 
 		$addon = json_decode(file_get_contents($url));
 		
-		if(empty($addon))
-		{
-			if($name != "Home")
-			{
+		if(empty($addon)){
+			if($name != "Home"){
 				header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
 				die("404 #$name Not Found");
-			} 
-			
+			}
+
 			$addon = (object) array(
 				"name" =>  $name,
 				"description" => "This is the home page for the «". $config->host ."» host. The Shebang snapshot for this page is not set.",
@@ -60,20 +55,18 @@ function shebang(&$config){
 			);
 		}
 		// set title with the Bar addon name
-		else
-		{
-			if($name != "Home")
-			{
+		else{
+			if($name != "Home"){
 				$config->head->title = $addon->name;
-			} 	
-		} 
-		
+			}
+		}
+
 		// add bar
 		$config->bars->$name = $addon;
-		
+
 		// set description
 		$config->head->description = $addon->description;
-		
+
 		// Headline Bar
 		$config->bars->Headline = (object) array(
 			"items"=> array(
@@ -81,9 +74,9 @@ function shebang(&$config){
 				(object) array( "el"=> "span", "text" => $addon->description)
 			)
 		);
-		
+
 		return;
-		
+
 		// debug
 		echo "<pre>";
 		$matches[] = "#!" . $matches[1];
