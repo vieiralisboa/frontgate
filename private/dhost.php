d9922c6c57377bdbd5a9a97d7353d8217dcd9aa0
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2015-01-26 13:19:59 +0000

    Add alias

--- private/dhost.php
+++ private/dhost.php
@@ -1,7 +1,26 @@
 <?php
 
+$server_name = null;
+
+//TODO forwarding (from JSON)
+
+//-------
+// Alias
+//-------
+$Alias = json_decode(file_get_contents(dirname(__FILE__)."/alias.json"));
+foreach($Alias as $host => $alias) {
+    foreach($alias as $name) if($name == $_SERVER['SERVER_NAME']) {
+        $server_name = $host;
+        break;
+    }
+    if(!empty($server_name)) break;
+}
+
+if(empty($server_name)) $server_name = $_SERVER['SERVER_NAME'];
+
+
 // Dynamic Host filename
-$json = "{$_SERVER['SERVER_NAME']}.json";
+$json = $server_name.".json";
 $jsonfile = dirname(__FILE__)."/json/".$json;
 
 // Default Host fallback
